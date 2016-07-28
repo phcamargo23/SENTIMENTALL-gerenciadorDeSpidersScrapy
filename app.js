973854422891a0a50c7d8e0d86275557c225e328
phcamargo23 <phcamargo23@gmaill.com>
2016-07-28 09:33:01 -0300

    MVP: listando projetos, spiders, jobs e arquivos.

--- app.js
+++ app.js
@@ -1,141 +1,121 @@
-(function(){
+(function () {
 
-angular.module('cidades', [])
-.controller('CidadesController', function($scope, $http) {
-	var scrapyd_url = 'http://localhost:6800/';
-	var scrapyd_php_url = 'http://localhost/app-cidades-http/';
+    angular.module('cidades', [])
+        .controller('CidadesController', function ($scope, $http) {
+            var scrapydUrl = 'http://localhost:6800/';
+            var scrapydUiUrl = 'http://localhost/estagio/';
 
+            listProjects();
 
-	$http.get('http://localhost:6800/listprojects.json').success(function(dados){
-        $scope.projects = dados.projects;
-    });
+            function listProjects() {
+                $http.get('http://localhost:6800/listprojects.json').success(function (dados) {
+                    $scope.projects = dados.projects;
+                });
+            }
 
-	$scope.selectProject = function(project) {
-		$http({
-			url: 'http://localhost:6800/listspiders.json',
-			method: "GET",
-			params: {project: project}
-		 }).success(function(dados){
-			 //console.log(dados);
-			$scope.spiders = dados.spiders;
-			//alert("Error : "+JSON.stringify(dados));
-		});
+            $scope.listSpidersAndJobs = function (project) {
+                $scope.listSpiders(project);
+                $scope.listJobs(project);
+            }
 
-		$http({
-			url: 'http://localhost:6800/listjobs.json',
-			method: "GET",
-			params: {project: project}
-		 }).success(function(dados){
-			$scope.jobsPending = dados.pending;
-			$scope.jobsRunning = dados.running;
-			$scope.jobsFinished = dados.finished;
-			//$scope._project = project;
-		});
+            $scope.listSpiders = function (project) {
 
-		$scope._project = project;
+                $http({
+                    url: 'http://localhost:6800/listspiders.json',
+                    method: "GET",
+                    params: {project: project}
+                }).success(function (dados) {
+                    //console.log(dados);
+                    $scope.spiders = dados.spiders;
+                    $scope._project = project;
+                    //alert("Error : "+JSON.stringify(dados));
+                });
+            };
 
-	};
+            $scope.listJobs = function (project) {
 
-	$scope.selectSpider = function(spider){
-		$scope._spider = spider;
+                $http({
+                    url: 'http://localhost:6800/listjobs.json',
+                    method: "GET",
+                    params: {project: project}
+                }).success(function (dados) {
+                    $scope.jobsPending = dados.pending;
+                    $scope.jobsRunning = dados.running;
+                    $scope.jobsFinished = dados.finished;
+                });
 
-		get_file(scrapyd_url+"items/"+$scope._project+"/"+$scope._spider);
-		//get_file('http://localhost:6800/items/tutorial/lab/');
-	}
+            };
 
+            $scope.listFiles = function (spider) {
 
-		function get_file(file){
-			$http({
-				url: scrapyd_php_url+'teste.php',
-				method: "POST",
-				params: {
-						'file':file
-				},
-				dataType: 'text'
-			 }).then(function(response){
-				if(response.status == "200")
-				{
-					//console.log(JSON.stringify(data));
-					//console.log(JSON.parse(response.data));
-					//$scope.files = JSON.parse(response.data);
-					$scope.files = response.data;
-				}
-				else
-				{
-						//alert("Error while scheduling job : " + response.data.message );
-						console.log(response);
-				}
-			});
+                var file = scrapydUrl + "items/" + $scope._project + "/" + spider;
 
-		}
+                $http({
+                    url: scrapydUiUrl + 'getFile.php',
+                    dataType: 'text',
+                    method: "POST",
+                    params: {
+                        'file': file
+                    }
+                }).then(function (response) {
+                    if (response.status == "200") {
+                        //console.log(JSON.stringify(data));
+                        //console.log(JSON.parse(response.data));
+                        //$scope.files = JSON.parse(response.data);
+                        $scope.files = response.data;
+                    }
+                    else {
+                        //alert("Error while scheduling job : " + response.data.message );
+                        console.log(response);
+                    }
+                });
 
-		function showFile(file){
-			file = scrapyd_url+"items/"+$scope._project+"/"+$scope._spider+"/"+file;
+                $scope._spider = spider;
+                //get_file(scrapydUrl + "items/" + $scope._project + "/" + $scope._spider);
+            }
 
-			$http({
-				url: scrapyd_php_url+'teste.php',
-				method: "POST",
-				params: {
-						'file':file
-				},
-				dataType: 'text'
-			 }).then(function(response){
-				if(response.status == "200")
-				{
-					//console.log(JSON.stringify(data));
-					//console.log(JSON.parse(response.data));
-					//$scope.files = JSON.parse(response.data);
-					$scope.files = response.data;
-				}
-				else
-				{
-						//alert("Error while scheduling job : " + response.data.message );
-						console.log(response);
-				}
-			});
+            function showFile(file) {
+                file = scrapydUrl + "items/" + $scope._project + "/" + $scope._spider + "/" + file;
 
-		}
+                $http({
+                    url: scrapydUiUrl + 'getFile.php',
+                    method: "POST",
+                    params: {
+                        'file': file
+                    },
+                    dataType: 'text'
+                }).then(function (response) {
+                    if (response.status == "200") {
+                        //console.log(JSON.stringify(data));
+                        //console.log(JSON.parse(response.data));
+                        //$scope.files = JSON.parse(response.data);
+                        $scope.files = response.data;
+                    }
+                    else {
+                        //alert("Error while scheduling job : " + response.data.message );
+                        console.log(response);
+                    }
+                });
 
-	$scope.schedule = function(spider) {
-		$http({
-			url: 'http://localhost:6800/schedule.json',
-			method: "POST",
-			//params: {project: $scope._project, spider : spider}
-			params: {project: $scope._project, spider : spider, setting : 'JOBDIR=C:/scrapyd/state'}
-		 }).then(function(response){
-			if(response.data.status === "ok")
-			{
-				$scope.listSpidersAndJobs($scope._project);
+            }
 
-			}
-			else
-			{
-				//alert("Error while scheduling job : " + JSON.stringify(dados) );
-				alert("Error while scheduling job : " + response.data.message );
-			}
-		});
-	};
+            $scope.schedule = function (spider) {
+                $http({
+                    url: 'http://localhost:6800/schedule.json',
+                    method: "POST",
+                    //params: {project: $scope._project, spider : spider}
+                    params: {project: $scope._project, spider: spider, setting: 'JOBDIR=C:/scrapyd/state'}
+                }).then(function (response) {
+                    if (response.data.status === "ok") {
+                        $scope.listSpidersAndJobs($scope._project);
 
-	$scope.scheduleJob = function(spider, jobid) {
-		$http({
-			url: 'http://localhost:6800/schedule.json',
-			method: "POST",
-			params: { project:$scope._project, spider:spider, jobid:jobid }
-		 }).then(function(response){
-			if(response.data.status === "ok")
-			{
-				$scope._spider = spider;
-				$scope.listSpidersAndJobs($scope._project);
-
-			}
-			else
-			{
-				//alert("Error while scheduling job : " + JSON.stringify(dados) );
-				alert("Error while scheduling job : " + response.data.message );
-			}
-		});
-	};
-
-});
+                    }
+                    else {
+                        //alert("Error while scheduling job : " + JSON.stringify(dados) );
+                        alert("Error while scheduling job : " + response.data.message);
+                    }
+                });
+            };
+        });
 
 })();
---
+++ getFile.php
@@ -0,0 +1,42 @@
+<?php
+if (isset($_REQUEST['file'])) {
+    $file = $_REQUEST['file'];
+
+
+    function get_text($filename)
+    {
+
+        $fp_load = fopen("$filename", "rb");
+
+        if ($fp_load) {
+
+            $content = '';
+            while (!feof($fp_load)) {
+                $content .= fgets($fp_load, 8192);
+            }
+
+            fclose($fp_load);
+
+            return $content;
+
+        }
+    }
+
+
+    $matches = array();
+
+    preg_match_all("/(a href\=\")([^\?\"]*)(\")/i", get_text($file), $matches);
+
+
+    $a = array();
+    foreach ($matches[2] as $match) {
+        //echo $match . '<br>';
+        array_push($a, $match);
+    }
+    //print_r($a);
+    echo json_encode($a);
+} else {
+    echo "Parameter file is missing !";
+}
+
+?>
